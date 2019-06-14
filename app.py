661caba4593212cc3a9c7c551505839527bcc209
Peter Buckley <pbuckley@chewy.com>
2019-06-14 00:19:23 -0400

    simple auth for admin page

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ This file creates your application.
 
 import os
 import requests
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
 from time import time
 import pymongo
 import json
 from email_validator import validate_email, EmailNotValidError
 from flask_recaptcha import ReCaptcha
 from flask_sslify import SSLify
+from functools import wraps
+
 
 app = Flask(__name__)
 
@@ -23,7 +25,7 @@ if 'DYNO' in os.environ: # only trigger SSLify if the app is running on Heroku
 else:
     app.debug = True
 
-app.config['ADMIN_PASS'] = os.environ.get('ADMIN_PASS', 'shouldda_set_admin_pass_%s' % (time()))
+ADMIN_PASS = os.environ.get('ADMIN_PASS', 'shouldda_set_admin_pass_%s' % (time()))
 
 # recaptcha config
 app.config['RECAPTCHA_SITE_KEY'] = os.environ.get('RECAPTCHA_SITE_KEY', 'shouldda_set_recaptcha_site_key')
@@ -60,6 +62,29 @@ with open('static/top100cities.json') as f:
     TOP_HUNDRED_LIST = json.load(f)
 
 # TOP_HUNDRED_LIST = [ 'Bahstin', 'Nawyawk', 'Zebbs' ]
+###
+# functions that need to be def'd prior to routes
+###
+
+def check_auth(username, password):
+    """This function is called to check if a username / password combination is valid."""
+    return username == 'nicholas' and password == ADMIN_PASS
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 ###
 # Routing for your application.
@@ -104,6 +129,7 @@ def home_post():
 
 
 @app.route('/admin/')
+@requires_auth
 def admin():
     """Render the bulk send form page."""
     return render_template('bulkform.html')
