50c9b780a7b27ee63d74a9559e650e2e48beedb9
Peter Buckley <dx-pbuckley@users.noreply.github.com>
2020-05-15 11:17:38 -0400

    Weather app (#2)
    * effecting any html changes requires removing the js block
    
    * this is how we path to static/img
    
    * a simple text form to start with
    
    * some mailgunnishness
    
    * no more print-o api-o
    
    * it does email and badly formatted location
    
    * pretty up that css
    
    * fix the form input back to zipcode
    
    * some formishness not quite working but also mongo WIP
    
    * add pymongo requirement
    
    * db is named emailaddresses the stringystring
    
    * now with logging in debug mode
    
    * city list into render_template :headdesk:
    
    * some sanity restored with listcomp
    
    I really wanted the top hunnert to be dict
    
    * very basic weather fetch working, WIP
    
    * top100cities by pop from wikipedia
    
    * add top100cities.json and load it
    
    * going with a fixed mongodb name
    
    * some better names for urls and templates
    
    * returning weather temp and conditions
    
    * convert temp to Farenheit, add precip to return
    
    * check the avg of next 16 days forecast and compare against it
    
    * enforce unique email constraint in the db
    
    * alreadysignedup render_template and flow
    
    * email_validator and signupsuccess template
    
    * try/except and bulksent, bulkfail templates for admin sending page
    
    * trying flask_recaptcha
    
    * cleanup after recaptcha v2 started working localhost
    
    * missed the )
    
    * try flask-sslify to fix url_for
    
    * default / to signup and add a footer with links
    
    * signup -> home in url_for in templates
    
    * simple auth for admin page
    
    * cleanup doc strings and remove dead comments
    
    * fix missing single quote
    
    * remove unused templates
    
    * center the recaptcha
    
    * fix site title from default
    
    * really send email now
    
    * fix alignment
    
    * switch from app.logger to print stdout for heroku logs
    
    * non-opted-in emails getting queued or not attempted?
    
    * the requests.post to mailgun is a black hole?
    
    * where am I losing the execution?
    
    * when precip was None we attempted a > compare to 0 :smh:
    
    oh Anchorage, Alaska; you troubled me
    
    Co-authored-by: Peter Buckley <pbuckley@chewy.com>

--- app.py
+++ app.py
@@ -7,27 +7,227 @@ This file creates your application.
 """
 
 import os
-from flask import Flask, render_template, request, redirect, url_for
+import requests
+from flask import Flask, render_template, request, redirect, url_for, Response
+from time import time
+import pymongo
+import json
+from email_validator import validate_email, EmailNotValidError
+from flask_recaptcha import ReCaptcha
+from flask_sslify import SSLify
+from functools import wraps
+
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+if 'DYNO' in os.environ: # only trigger SSLify if the app is running on Heroku
+    sslify = SSLify(app)
+else:
+    app.debug = True
 
+ADMIN_PASS = os.environ.get('ADMIN_PASS', 'shouldda_set_admin_pass_%s' % (time()))
+
+# recaptcha config
+app.config['RECAPTCHA_SITE_KEY'] = os.environ.get('RECAPTCHA_SITE_KEY', 'shouldda_set_recaptcha_site_key')
+app.config['RECAPTCHA_SECRET_KEY'] = os.environ.get('RECAPTCHA_SECRET_KEY', 'shouldda_set_recaptcha_secret_key')
+app.config['RECAPTCHA_SIZE'] = 'compact'
+app.config['RECAPTCHA_THEME'] = 'dark'
+
+recaptcha = ReCaptcha(app=app)
+
+MONGODB_URI = os.environ.get('MONGODB_URI', 'shouldda_set_that_mongodb_uri')
+MG_API_KEY = os.environ.get('MAILGUN_API_KEY', 'shouldda_set_that_mg_api_key')
+MG_DOMAIN = os.environ.get('MAILGUN_DOMAIN', 'shouldda_set_that_mg_domain')
+MG_API_URL='https://api.eu.mailgun.net/v3/%s/messages' % MG_DOMAIN
+
+WEATHERBIT_API_KEY = os.environ.get('WEATHERBIT_API_KEY', 'shouldda_set_that_wb_api_key')
+WEATHERBIT_API_URL = 'https://api.weatherbit.io/v2.0/current'
+WEATHERBIT_FORECAST_URL = 'https://api.weatherbit.io/v2.0/forecast/daily'
+
+CLIENT = pymongo.MongoClient(MONGODB_URI)
+DB = CLIENT['heroku_xncgtv8c']
+DB.emaddrcol.create_index(('email'), unique=True)
+
+# sunny or +5 degrees
+NICE_PAIR = { "subject": "It's nice out! Enjoy a discount on us!",
+              "phrasing": "Hope you are enjoying the" }
+# either precipitating or 5 degrees cooler than the average
+NOTNICE_PAIR = { "subject": "Not so nice out? That's okay, enjoy a discount on us!",
+                 "phrasing": "Take a break from the" }
+AVG_PAIR = { "subject": "Enjoy a discount on us!",
+             "phrasing": "Fine with the" }
+
+with open('static/top100cities.json') as f:
+    TOP_HUNDRED_LIST = json.load(f)
+
+###
+# functions that need to be def'd prior to routes
+###
+
+def check_auth(username, password):
+    """This function is called to check if a username / password combination is valid."""
+    return username == 'nicholas' and password == ADMIN_PASS
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 ###
 # Routing for your application.
 ###
 
+
 @app.route('/')
 def home():
-    """Render website's home page."""
-    return render_template('home.html')
+    """Render the signup page."""
+    return render_template('emailform.html', top_hundred_cities=[x['name'] for x in TOP_HUNDRED_LIST])
 
 
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
+@app.route('/', methods=['POST'])
+def home_post():
+    """Signup an email address and city."""
+    if recaptcha.verify():
+        # SUCCESS
+        print("Passed recaptcha")
+        pass
+    else:
+        # FAILED
+        print("Failed recaptcha")
+        return render_template('404.html')
+    emailaddy = request.form['emailaddress']
+    city = request.form['city_name']
+    try:
+        v = validate_email(emailaddy) # validate and get info
+        normalizedemail = v["email"] # replace with normalized form
+        print("Successfully validated: %s | normalized as: %s" % (emailaddy, normalizedemail))
+    except EmailNotValidError as e:
+        # email is not valid, exception message is human-readable
+        print("Invalid email %s: %s" % (emailaddy, str(e)))
+        return render_template('invalidemail.html', errmsg=(str(e)), youremail=emailaddy)
+
+    one_record = {'email': normalizedemail, 'city': city }
+    try:
+        DB.emaddrcol.insert_one(one_record)
+        print("Inserting into db: %s / %s from client %s \n: record email %s, city %s" % (DB, DB.emaddrcol, CLIENT, one_record['email'], one_record['city']))
+    except:
+        print("Duplicate email tried to insert into db: %s" % (normalizedemail))
+        return render_template('alreadysignedup.html', youremail=normalizedemail)
+    return render_template('signupsuccess.html', youremail=normalizedemail, yourcity=city)
+
+
+@app.route('/admin/')
+@requires_auth
+def admin():
+    """Render the bulk send form page."""
+    return render_template('bulkform.html')
+
+
+@app.route('/admin/', methods=['POST'])
+def admin_post():
+    """ Submit the bulk email send form."""
+    try:
+        total_sent = send_bulk_emails()
+    except:
+        return render_template('bulkfail.html')
+    return render_template('bulksent.html', total_sent=total_sent)
+
+
+# just mah functionsz
+
+def farenheit(ctemp):
+    """ Convert celcius to farenheit."""
+    return round(9.0/5.0 * ctemp + 32)
+
+
+def avg_based_on_forecast(city):
+    """Calculate the avg temp based on next 16 day forecast."""
+    wparams = { 'city': city,
+                'key': WEATHERBIT_API_KEY
+    }
+    resp = requests.get(WEATHERBIT_FORECAST_URL, params=wparams)
+    alltemps = [farenheit(x['temp']) for x in json.loads(resp.text)['data']]
+    return round(sum(alltemps) / len(alltemps))
+
+
+def fetch_weather(city):
+    """Fetch current weather for given city."""
+    wparams = { 'city': city,
+                'key': WEATHERBIT_API_KEY
+    }
+    resp = requests.get(WEATHERBIT_API_URL, params=wparams)
+    # this works, need to likely raise for status?
+    full_weather = json.loads(resp.text)
+    print("Got full_weather: %s" % (full_weather))
+    if not full_weather['data'][0]['precip']:
+        print("Precip was None, coercing to 0")
+        full_weather['data'][0]['precip'] = 0
+    weather_dict = {
+        'temp': farenheit(full_weather['data'][0]['temp']),
+        'conditions': full_weather['data'][0]['weather']['description'].lower(),
+        'precip': full_weather['data'][0]['precip'],
+        'forecast_temp': avg_based_on_forecast(city)
+    }
+    print("Trimmed down weather_dict: %s" % (weather_dict))
+    return weather_dict
+
+
+def subject_phrase_picker(city):
+    weather = fetch_weather(city)
+    print("Picking our weather-dependent phrase for weather %s and city %s" % (weather, city))
+    if weather['precip'] > 0 or weather['temp'] <= (weather['forecast_temp'] - 5):
+        print("Notnice weather discovered")
+        phrase = NOTNICE_PAIR
+    elif weather['temp'] >= (weather['forecast_temp'] + 5):
+        print("Nice weather discovered")
+        phrase = NICE_PAIR
+    else:
+        print("Avg weather discovered")
+        phrase = AVG_PAIR
+    print("Phrase chosen: %s" % (phrase))
+    print("Weather determined to be: %s and %s" % (weather['temp'], weather['conditions']))
+    return phrase, "%s and %s" % (weather['temp'], weather['conditions'])
+
+
+def send_sbemail(email_addy, city):
+    """Send a single email, formatted with weather and phrasing to our liking."""
+    print("Single email send attempt: %s, %s" % (email_addy, city))
+    given_pair, weather = subject_phrase_picker(city)
+    mailtext = '%s %s in %s!' % ( given_pair['phrasing'], weather, city)
+    print("Going to try a mailgun post to: %s with subject %s" % (email_addy, given_pair['subject']))
+    response = requests.post(
+        MG_API_URL,
+        auth=("api", MG_API_KEY),
+        data={'from': 'Nick Cage <nickcage@%s>' % (MG_DOMAIN),
+              'to': [email_addy],
+              'subject': given_pair['subject'],
+              'text': mailtext
+        })
+    print("status: %s | %s and response: %s" % (response.status_code, response.reason, response.text))
+    print("Attempted send %s to: %s!" % (mailtext, email_addy))
+    return "Sent %s to: %s!" % (mailtext, email_addy)
+
+
+def send_bulk_emails():
+    """Send multiple emails."""
+    email_count = 0
+    for email in DB.emaddrcol.find(): # collection:
+        print("About to send to %s" % (email['email']))
+        send_sbemail(email['email'], email['city'])
+        email_count += 1
+    print("Processed %s email attempts" % (email_count))
+    return email_count
 
 
 ###
